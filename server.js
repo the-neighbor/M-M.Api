00eda4144a7a74eb3113aafd652d19289f94a455
Olive Daly <olive-branch@posteo.me>
2022-11-03 10:28:37 -0400

    implement JWT based authentication middleware

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 const express = require('express');
 
+const jwt = require('jsonwebtoken');
+const secret = 'testingsecret';
+const expiration = '1h';
+
+const bcrypt = require('bcrypt');
+
+function withAuth(req, res, next) {
+    console.log(req.headers)
+    let token = req.body.token || req.query.token || req.headers['authorization'];
+    console.log(token)
+    if (token) {
+        try {
+            const { user } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = user;
+            console.log('next');
+            next();
+        }
+        catch (err) {
+            res.status(401).json({message: 'Invalid token'});
+        }
+    } else {
+        res.status(401).json({message: 'No token provided'});
+        return req;
+    }
+}
+
+function signToken(user) {
+    const payload = { user };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
 main().catch(err => console.log(err));
 
 async function main() {
@@ -11,6 +42,9 @@ async function main() {
 
 
 //SCHEMA FOR USER MODEL
+
+
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -18,6 +52,10 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
       },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
@@ -38,8 +76,19 @@ const userSchema = new mongoose.Schema({
     profile: {
         type: mongoose.Schema.Types.ObjectId,
         ref:'Profile'
-    }
+    },
 });
+
+function checkPassword(attempt) {
+
+    const result = bcrypt.compareSync(attempt, this.passwordHash);
+    console.log(result);
+    console.log(attempt);
+    return result;
+
+}
+
+userSchema.methods.checkPassword = checkPassword;
 
 //POST MODEL
 const postSchema = new mongoose.Schema({
@@ -88,14 +137,26 @@ app.get("/users/:username", async (req, res) => {
 app.post("/users/create", async (req, res) => {
     console.log(req);
     var { username, email, password } = req.body;
-    const user = new User({username, email, password});
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = new User({username, email, passwordHash});
     await user.save();
     res.send(user);
 })
 
-app.post("/posts/create", async (req, res) => {
-    var { username, pid, title, content, imageUri, tags } = req.body;
-    const post = new Post({username, pid, title, content, imageUri, tags});
+app.post("/users/login", async (req, res) => {
+    var { username, password } = req.body;
+    const user = await User.findOne({username});
+    if (user && user.checkPassword(password)) {
+        const token = signToken(user);
+        res.json({token});
+    } else {
+        res.status(401).json({message: 'Invalid credentials'});
+    }
+})
+
+app.post("/posts/create", withAuth, async (req, res) => {
+    var { username, title, content, imageUri, tags } = req.body;
+    const post = new Post({username, title, content, imageUri, tags});
     await post.save();
     res.send(post);
 });
